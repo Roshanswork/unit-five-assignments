e28d0f4d28d96582f1f9513fbc0971ff48c6e621
Roshanswork <91541993+Roshanswork@users.noreply.github.com>
2022-01-31 21:26:57 +0530

    first assignment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,21 @@ import { useState } from "react";
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
 
-  const clickBtn = (value) => {
+  const incrementOne = (value) => {
     setCounter(counter + 1);
+  };
+  const decrementOne = (value) => {
+    setCounter(counter - 1);
+  };
+  const double = (value) => {
+    setCounter(counter * 2);
   };
   return (
     <div>
       <h2>Counter : {counter}</h2>
-      <button onClick={clickBtn}>Add 1</button>
+      <button onClick={incrementOne}>Add 1</button>
+      <button onClick={decrementOne}>Reduce 1</button>
+      <button onClick={double}>Double</button>
     </div>
   );
 };
